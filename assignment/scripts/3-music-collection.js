763e2ed23926455aceb121491079dad0724f60b4
Matt Dow <matt.dow.vo@gmail.com>
2021-09-13 18:07:24 -0500

    Added and tested albumSearch function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -32,6 +32,7 @@ function showCollection(array) {
 
 showCollection(collection);
 
+// Creating findByArtist
 function findByArtist(artist) {
   let resultsArray = []
   for (album of collection) { // looping through the albums in collection
@@ -55,3 +56,65 @@ showCollection(findByArtist('Cornelius'));
 
 console.log('Crazy Town albums in collection (should show none):');
 showCollection(findByArtist('Crazy Town'));
+
+// Creating albumSearch functions
+
+function albumSearch(searchCriteria) {
+  // console.log('In albumSearch');
+  let resultsArray = [];
+  // First checking to see if searchCriteria is empty, because we need to return
+  // the full collection in that case and not go through the function. To check
+  // for an empty object, I Googled for a solution and decided to use the
+  // Object.keys property, which should be an empty array if serachCriteria is
+  // empty.
+
+  if (searchCriteria === undefined || Object.keys(searchCriteria).length === 0) {
+    console.log('Search was empty, returning full collection');
+    return collection;
+    }
+  for (album of collection) { // looping through each album in the collection
+    // creating a conditional that checks for a match between both artist and year
+    // properties for searchCriteria and collection objects
+    // console.log(album.artist, album.yearPublished, 'vs', searchCriteria.artist, searchCriteria.year);
+    if (album.artist === searchCriteria.artist &&
+    album.yearPublished === searchCriteria.year) {
+      resultsArray.push(album);
+    } // end of criteria check
+  } // end of loop through the albums in collection
+  return resultsArray; // return the matches in the array
+} // end of albumSearch functions
+
+// I'll test this function by calling the results of albumSearch in the showCollection function,
+// since showCollection works for any array.
+
+// Testing for two albums in the collection that have multiple items with that year or artists
+
+console.log('2018 Jon Hopkins albums in collection (should show one titled Singularity):');
+showCollection((albumSearch({artist: 'Jon Hopkins', year: 2018})));
+
+console.log('1998 Outkast albums in collection (should show one titled Aquemini):');
+showCollection((albumSearch({artist: 'Outkast', year: 1998})));
+
+console.log('1998 Cornelius albums in collection (should show one titled Fantasma):');
+showCollection((albumSearch({artist: 'Cornelius', year: 1998})));
+
+// Testing for an album that would never be in my collection
+
+console.log('1999 Crazy Town albums in collection (should show none):');
+showCollection((albumSearch({artist: 'Crazy Town', year: 1999})));
+
+// Testing for searches that match only one searchCriteria
+
+console.log('2016 Jon Hopkins albums in collection (should show none):');
+showCollection((albumSearch({artist: 'Jon Hopkins', year: 2016})));
+
+console.log('2018 Mitski albums in collection (should show none):');
+showCollection((albumSearch({artist: 'Mitski', year: 1998})));
+
+// Testing for searches with nothing or an empty object.
+
+console.log('Testing albumSearch with no criteria (should show the full collection):');
+showCollection((albumSearch()));
+
+console.log('Testing albumSearch with empty criteria (should show the full collection):');
+showCollection((albumSearch({})));
