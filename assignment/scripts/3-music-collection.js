e3cc24d07b07b882905ceddbbcd6a7d951b8d0fe
Matt Dow <matt.dow.vo@gmail.com>
2021-09-13 18:54:14 -0500

    Added tracks to albums, edited showCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,32 +2,49 @@ console.log('***** Music Collection *****')
 let collection = [];
 // establish empty array variable
 
-function addToCollection(albumTitle, albumArtist, albumYearPublished) {
+function addToCollection(albumTitle, albumArtist, albumYearPublished, albumTracks) {
   newAlbum = {
     title: albumTitle,
     artist: albumArtist,
-    yearPublished: albumYearPublished
+    yearPublished: albumYearPublished,
+    tracks: albumTracks
 // assign the variables given to the appropriate object properties
-  }
+};
   collection.push(newAlbum);
   // adding the new object to the collection array
   return newAlbum;
 }
 // adding some albums
-console.log('Added album to collection:', addToCollection('Double Negative', 'Low', 2018));
-console.log('Added album to collection:', addToCollection('Aquemini', 'Outkast', 1998));
-console.log('Added album to collection:', addToCollection('Singularity', 'Jon Hopkins', 2018));
-console.log('Added album to collection:', addToCollection('Fantasma', 'Cornelius', 1998));
-console.log('Added album to collection:', addToCollection('ATLiens', 'Outkast', 1996));
-console.log('Added album to collection:', addToCollection('Point', 'Cornelius', 2002));
+console.log('Added album to collection:', addToCollection('Double Negative', 'Low', 2018,
+[{name: 'Quorum', duration: '3:42'}, {name: 'Fly', duration: '5:48'},
+{name: 'Dancing and Blood', duration: '6:22'}]));
+console.log('Added album to collection:', addToCollection('Aquemini', 'Outkast', 1998,
+[{name: 'Hold On, Be Strong', duration: '1:12'}, {name:'Return of the G', duration: '4:49'},
+{name: 'Rosa Parks', duration: '5:24'}]));
+console.log('Added album to collection:', addToCollection('Singularity', 'Jon Hopkins', 2018,
+[{name: 'Singularity', duration: '6:29'}, {name: 'Emerald Rush', duration: '5:37'},
+{name: 'Neon Pattern Drum', duration: '6:07'}]));
+console.log('Added album to collection:', addToCollection('Fantasma', 'Cornelius', 1998,
+[{name: 'Mic Check', duration: '3:01'}, {name: 'The Micro...', duration: '3:37'},
+{name: 'New Music Machine', duration: '3:53'}]));
+console.log('Added album to collection:', addToCollection('ATLiens', 'Outkast', 1996,
+[{name: 'You May Die', duration: '1:05'}, {name: 'Two Dope Boyz', duration: '2:46'},
+{name: 'ATLiens', duration: '3:50'}]));
+console.log('Added album to collection:', addToCollection('Point', 'Cornelius', 2002,
+[{name: 'Bug', duration: '0:40'}, {name: 'Point of View Point', duration: '3:56'},
+{name: 'Smoke', duration: '5:50'}]));
 
 function showCollection(array) {
   // use the length property of the array to print the number of albums in the collection
   console.log(`Number of albums in collection is ${array.length}:`);
   for (album of array) {
     // calling each of the array properties in a template literal
-    console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
-  } // end of for loop
+    console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}:`);
+    // create another loop for all of the album tracks
+    for (let i = 0; i < album.tracks.length; i++) {
+      console.log(`${i+1}. NAME: ${album.tracks[i].name} DURATION: ${album.tracks[i].duration}`);
+    } // end of loop through tracks
+  } // end of loop through albums
 } // end of showCollection function
 
 showCollection(collection);
